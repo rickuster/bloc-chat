4ce6ba7ed68d62e2d44dcd87faa5bda9d0d84ce5
rickuster <rickuster@gmail.com>
2017-05-05 00:30:11 -0700

    list chat rooms incomplete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,11 +7,11 @@
          });
          
          $stateProvider
-             .state('main', {
-                 url: '/',
-                 controller: 'MainCtrl as main',
-                 templateUrl: '/templates/main.html'
-             })
+             .state('home', {
+                 url: '/', 
+                 controller: 'HomeCtrl as home',
+                 templateUrl: '/templates/home.html'
+             });
       }
  
      angular
