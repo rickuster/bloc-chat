15a3a27b9429ee1e7c17e45252e378937631e3fb
rickuster <rickuster@gmail.com>
2017-06-01 00:41:45 -0700

    completed send messages

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -3,11 +3,27 @@
         this.rooms = Room.all;
         this.roomName = null;
         this.message = null;
+        this.visibility = null;
+        this.roomId = null;
+        this.form = {};
         this.setRoom = function(roomName){
             this.roomName = roomName;
         }
         this.setMessages = function(id){
             this.message = Message.getByRoomId(id);
+        }
+        this.showDiv = function() {
+            this.visibility = true;
+        }
+        
+        this.sendMessage = function(text, id) {
+            Message.send(text, id);
+        }
+        this.setRoomId = function(id) {
+            this.roomId = id;
+        }
+        this.clearForm = function () {
+            this.testform.$setPristine();
         }
     }
 
--- app/scripts/services/message.js
+++ app/scripts/services/message.js
@@ -1,12 +1,19 @@
 (function() {
-  function Message($firebaseArray) {
+  function Message($firebaseArray, $cookies, $filter) {
     var Message = {};
     var ref = firebase.database().ref().child("messages");
     var messages = $firebaseArray(ref);
+    var d = new Date();
+
 
     Message.getByRoomId = function(roomId) {
         // Filter the messages by their room ID
         return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+    };
+      
+    Message.send = function(newMessage, id) {
+        // Send method logic
+        messages.$add({content: newMessage, roomId: id, username: $cookies.get('blocChatCurrentUser'), sentAt: $filter('date')(d, 'shortTime')});
     };
       
     return Message;
@@ -14,5 +21,5 @@
 
   angular
     .module('blocChat')
-    .factory('Message', ['$firebaseArray', Message]);
+    .factory('Message', ['$firebaseArray', '$cookies', '$filter', Message]);
 })();
