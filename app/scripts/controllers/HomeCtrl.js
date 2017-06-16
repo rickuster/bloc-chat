340b4dcc9c15028d88f61ffdd7f5bd243425e344
rickuster <rickuster@gmail.com>
2017-06-15 22:47:42 -0700

    last touch ups

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -3,27 +3,29 @@
         this.rooms = Room.all;
         this.roomName = null;
         this.message = null;
+        this.newMessage = null;
         this.visibility = null;
         this.roomId = null;
         this.form = {};
         this.setRoom = function(roomName){
             this.roomName = roomName;
         }
+        
         this.setMessages = function(id){
             this.message = Message.getByRoomId(id);
         }
+        
         this.showDiv = function() {
             this.visibility = true;
         }
         
-        this.sendMessage = function(text, id) {
-            Message.send(text, id);
+        this.sendMessage = function(text) {
+            Message.send(text, this.roomId); 
+            this.newMessage = null;
         }
+        
         this.setRoomId = function(id) {
             this.roomId = id;
-        }
-        this.clearForm = function () {
-            this.testform.$setPristine();
         }
     }
 
